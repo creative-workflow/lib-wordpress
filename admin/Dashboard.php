7684e4c1463bc59e152565c7addc92956774c39f
Tom Hanoldt <tom@creative-workflow.berlin>
2017-11-30 19:22:28 +0100

    make it static

--- admin/Dashboard.php
+++ admin/Dashboard.php
@@ -3,31 +3,29 @@
 namespace cw\wp\admin;
 
 class Dashboard{
-  public function addMainWidget($id, $name, $callback, $priotirty='core'){
-    return $this->addWidget($id, $name, $callback, 'normal', $priotirty);
+  public static function addMainWidget($id, $name, $callback, $priotirty='core'){
+    self::addWidget($id, $name, $callback, 'normal', $priotirty);
   }
 
-  public function addSideWidget($id, $name, $callback, $priotirty='core'){
-    return $this->addWidget($id, $name, $callback, 'side', $priotirty);
+  public static function addSideWidget($id, $name, $callback, $priotirty='core'){
+    self::addWidget($id, $name, $callback, 'side', $priotirty);
   }
 
-  public function addWidget($id, $name, $callback, $where='normal', $priotirty='core'){
+  public static function addWidget($id, $name, $callback, $where='normal', $priotirty='core'){
     add_action('wp_dashboard_setup', function() use($id, $name, $callback, $where, $priotirty){
       add_meta_box($id, $name, $callback, 'dashboard', $where, $priotirty);
     }, 1000);
-    return $this;
   }
 
-  public function removeWidgets(){
+  public static function removeWidgets(){
     add_action('wp_dashboard_setup', function(){
       global $wp_meta_boxes;
       $wp_meta_boxes['dashboard']['side'] = ['core' => []];
       $wp_meta_boxes['dashboard']['normal'] = ['core' => []];
     }, 1000);
-    return $this;
   }
 
-  public function replaceWelcomePanel($input){
+  public static function replaceWelcomePanel($input){
     add_action( 'admin_footer', function() use($input){
       if(is_callable($input))
         $input = $input();
@@ -37,10 +35,9 @@ class Dashboard{
 
       echo '<script>(function($){$("#welcome-panel").html("'.$input.'");})(jQuery);</script>';
     }, 1000);
-    return $this;
   }
 
-  public function appendToWelcomePanel($input){
+  public static function appendToWelcomePanel($input){
     add_action( 'admin_footer', function() use($input){
       if(is_callable($input))
         $input = $input();
@@ -50,13 +47,11 @@ class Dashboard{
 
       echo '<script>(function($){$("#welcome-panel").append("'.$input.'");})(jQuery);</script>';
     }, 1000);
-    return $this;
   }
 
-  public function hideWidgetArea(){
+  public static function hideWidgetArea(){
     add_action( 'admin_footer', function(){
       echo '<script>(function($){$("#dashboard-widgets-wrap").html("");})(jQuery);</script>';
     }, 101 );
-    return $this;
   }
 }
