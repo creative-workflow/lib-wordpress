cdd68c351b64dacfeb4709fd28ec7cedb814c5b3
Tom Hanoldt <tom@creative-workflow.berlin>
2018-11-02 17:00:38 +0100

    add typeUpload

--- custom/MetaBox.php
+++ custom/MetaBox.php
@@ -85,6 +85,11 @@ class MetaBox{
     return $this;
   }
 
+  public function typeUpload(){
+    $this->type = 'media';
+    return $this;
+  }
+
   public function typeText(){
     $this->type = 'text';
     return $this;
@@ -126,10 +131,17 @@ class MetaBox{
     return $this->default;
   }
 
+  public function contentAsImage($size=null){
+    return wp_get_attachment_image_src($this->content(), $size);
+  }
+
   public function show($post, $metabox){
+    wp_nonce_field(plugin_basename(__FILE__), $this->metaId().'_nounce');
+
     $content = $this->content($post);
     if($this->description)
-      echo '<i>'.$this->description.'</i><br>';
+      echo '<p class="description">'.$this->description.'</p>';
+
     switch($this->type){
       case 'html':
         wp_editor($content, $this->metaId(), array(
@@ -149,6 +161,52 @@ class MetaBox{
 
         echo '</select>';
       break;
+      case 'media':
+        echo '<button type="button" class="button" id="'.$this->metaId().'_button" data-media-uploader-target="#'.$this->metaId().'">upload</button>';
+        echo '<input type="hidden" id="'.$this->metaId().'" name="'.$this->metaId().'" value="'.$content.'">';
+        echo '<a href="#" id="'.$this->metaId().'_button_remove" style="display:inline-block; padding-left: 10px">delete</a>';
+        if($content)
+          $image = $this->contentAsImage('50x50');
+        else
+          $image = [0 => '#'];
+
+        echo '<img id="'.$this->metaId().'_image" style="width: 50px; height: auto; display: block;" src="'.$image[0].'"/>';
+
+        add_action( 'admin_footer', function(){
+          ?><script type="text/javascript">
+            jQuery(function($){
+              $('body').on('click', '#<?= $this->metaId().'_button' ?>', function(e){
+                e.preventDefault();
+
+            var button = $(this),
+                custom_uploader = wp.media({
+                  title: 'Insert image',
+                  library : {
+                    uploadedTo : wp.media.view.settings.post.id,
+                    type : 'image'
+                  },
+                  button: {
+                    text: 'Use this image' // button label text
+                  },
+                  multiple: false
+                }).on('select', function() {
+                  var attachment = custom_uploader.state().get('selection').first().toJSON();
+                  $('#<?= $this->metaId().'_image' ?>').attr('src', attachment.url);
+                  $('#<?= $this->metaId() ?>').val(attachment.id);
+                })
+                .open();
+              });
+
+              $('body').on('click', '#<?= $this->metaId().'_button_remove' ?>', function(){
+                $('#<?= $this->metaId().'_image' ?>').attr('src', '#');
+                $('#<?= $this->metaId() ?>').val('');
+                return false;
+              });
+            });
+          </script><?
+        });
+
+      break;
       case 'text':
       case 'date':
         if(is_a($content, 'DateTime'))
@@ -160,14 +218,36 @@ class MetaBox{
   }
 
   public function save($post_id, $post){
-    if(!isset($_POST[$this->metaId()]))
+    if(defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)
       return ;
 
-    if($_POST[$this->metaId()] != '')
-      update_post_meta($post_id, $this->metaId(), $_POST[$this->metaId()]);
+    if(!current_user_can('edit_page', $post_id))
+       return ;
 
-    else
+    if(!wp_verify_nonce($_POST[$this->metaId().'_nounce'], plugin_basename(__FILE__)))
+      return ;
+
+    if($_POST[$this->metaId()] == ''){
       delete_post_meta($post_id, $this->metaId());
+      return ;
+    }
+
+    if($this->type != 'upload'){
+      update_post_meta($post_id, $this->metaId(), $_POST[$this->metaId()]);
+      return ;
+    }
+
+    if(empty($_FILES[$this->metaId()]['name']))
+      return ;
+
+    $upload = wp_upload_bits($_FILES[$this->metaId()]['name'], null, file_get_contents($_FILES[$this->metaId()]['tmp_name']));
+
+    if(isset($upload['error']) && $upload['error'] != 0) {
+      wp_die('There was an error uploading your file. The error is: ' . $upload['error']);
+      return ;
+    }
+
+    update_post_meta($post_id, $this->metaId(), $upload);
   }
 
   public function publish(){
